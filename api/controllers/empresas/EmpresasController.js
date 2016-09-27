d9da86c0f541c58365fd196a172da2d2cd7d8610
Jose Miguel Soto Acosta <jomisoac@gmail.com>
2016-09-27 09:48:16 -0500

    update logo and firma digital

--- api/controllers/empresas/EmpresasController.js
+++ api/controllers/empresas/EmpresasController.js
@@ -5,6 +5,7 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 const _ = require('lodash');
+const uid = require('uid-safe')
 
 module.exports = {
     identity: 'Empresas',
@@ -24,8 +25,59 @@ module.exports = {
             })
             .catch(error => {
                 if (!error.invalidAttributes.username && data.user.username) {
-                    User.destroy({username: data.user.username}).exec(() => {});
+                    User.destroy({username: data.user.username}).exec(() => {
+                    });
                 }
             })
     },
+
+    saveLogo(req, res){
+        Empresas.findOne({id: req.allParams().id})
+            .then((empresa) => {
+                if (empresa) {
+                    req.file('logo').upload({
+                            dirname: sails.config.appPath + '/public/images/empresas',
+                            saveAs: function (__newFileStream, cb) {
+                                cb(null, empresa.logo || uid.sync(18) + empresa.id + '.' + _.last(__newFileStream.filename.split('.')));
+                            }
+                        },
+                        (error, uploadedFiles) => {
+                            if (error) return res.negotiate(error);
+                            if (!uploadedFiles[0]) return res.badRequest('ha ocurrido un erro inesperado al almacenar la imagen');
+                            const filename = _.last(uploadedFiles[0].fd.split('/'));
+                            empresa.logo = filename;
+                            empresa.save((err, s) => res.ok('files upload'));
+                        }
+                    );
+                } else {
+                    return res.notFound('la empresa no existe');
+                }
+            }).catch(res.negotiate);
+    },
+
+    saveFirma(req, res){
+        Empresas.findOne({id: req.allParams().id})
+            .then((empresa) => {
+                if (empresa) {
+                    console.log(req.file('firmaDigital'));
+                    req.file('firmaDigital').upload({
+                            dirname: sails.config.appPath + '/public/images/empresas/firma',
+                            saveAs: function (__newFileStream, cb) {
+                                cb(null, empresa.firma_digital || uid.sync(18) + empresa.id + '.'
+                                    + _.last(__newFileStream.filename.split('.')));
+                            }
+                        },
+                        (error, uploadedFiles) => {
+                            if (error) return res.negotiate(error);
+                            if (!uploadedFiles[0]) return res.badRequest('ha ocurrido un erro inesperado al almacenar la imagen');
+                            const filename = _.last(uploadedFiles[0].fd.split('/'));
+                            empresa.firma_digital = filename;
+                            empresa.save((err, s) => res.ok('files upload'));
+                        }
+                    );
+                } else {
+                    return res.notFound('la empresa no existe');
+                }
+            }).catch(res.negotiate);
+    }
 };
