185d3b76480b69ee8fa73a7e37962177f1756586
Gustavo Pérez Machado <gustperz@gmail.com>
2016-09-28 15:42:17 -0500

    turnos rutas

--- api/controllers/rutas/RutasController.js
+++ api/controllers/rutas/RutasController.js
@@ -8,32 +8,33 @@ module.exports = {
 
     updateTurnos(req, res) {
         const ruta_id = req.params.id;
-        TurnosRuta.destroy({ruta: ruta_id}).then(() => {
-            const turnos = req.allParams();
+        TurnosRuta.query('DELETE FROM `turnosruta` WHERE ruta = '+ruta_id, (err) => {
+            if (err) return res.negotiate(err);
+            const turnos = req.allParams().turnos;
             _.forEach(turnos, function (turno) {
                 TurnosRuta.create({
                     pos: turno.pos,
                     conductor: turno.conductor,
                     ruta: ruta_id
                 }).then(() => {
-                    sails.sockets.broadcast('conductor'+turno.conductor+'watcher', 'turnoUpdate', {pos: turno.pos});
+                    sails.sockets.broadcast('conductor' + turno.conductor + 'watcher', 'turnoUpdate', {pos: turno.pos});
                 });
             });
-            return res.ok()
-        }).catch(res.negotiate);
+            return res.ok();
+        });
     },
 
     populateTurnos(req, res) {
         const ruta_id = req.params.id;
         TurnosRuta.find({ruta: ruta_id}).populate('conductor')
             .then((turnos) => {
-                return res.ok( _.map( turnos, _.partialRight( _.pick, [
-                    'identificacion',
-                    'nombres',
-                    'apellidos',
-                    'imagen',
-                    'codigo_vial_vehiculo'
-                ] ) ) );
+                console.log(turnos)
+                return res.ok( _.map( turnos, turno => {
+                    return {
+                        pos: turno.pos,
+                        conductor: _.pick(turno.conductor, ['id, identificacion', 'nombres', 'apellidos', 'imagen', 'codigo_vial_vehiculo'])
+                    }
+                }) );
             }).catch(res.negotiate);
     }
 
