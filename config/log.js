1dbfaa193daf88b1e1875e24eb2e2560e622539a
Gustavo Pérez Machado <gustperz@gmail.com>
2016-09-29 22:50:49 -0500

    application logging

--- config/log.js
+++ config/log.js
@@ -2,21 +2,24 @@
  * Built-in Log Configuration
  */
 
+const winston = require('winston');
+const path = require('path');
+
+const logger = new(winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({}),
+    new (require('winston-daily-rotate-file'))({
+      dirname: path.resolve('logs'),
+      datePattern: '.yyyy-MM-dd.log',
+      filename: 'vs',
+      prettyPrint: true,
+      timestamp: true,
+      level: 'error'
+    })
+  ]
+});
+
 module.exports.log = {
-
-  /***************************************************************************
-  *                                                                          *
-  * Valid `level` configs: i.e. the minimum log level to capture with        *
-  * sails.log.*()                                                            *
-  *                                                                          *
-  * The order of precedence for log levels from lowest to highest is:        *
-  * silly, verbose, info, debug, warn, error                                 *
-  *                                                                          *
-  * You may also set the level to "silent" to suppress all logs.             *
-  *                                                                          *
-  ***************************************************************************/
-
-  level: 'verbose'
-  
-
+  level: 'verbose',
+  custom: logger
 };
