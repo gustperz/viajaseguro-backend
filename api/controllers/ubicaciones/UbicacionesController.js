83663fdb3988854c504b3aa1650c0909378d190c
Gustavo Pérez Machado <gustperz@gmail.com>
2016-11-29 18:07:29 -0500

    change pos conducteres event name

--- api/controllers/ubicaciones/UbicacionesController.js
+++ api/controllers/ubicaciones/UbicacionesController.js
@@ -11,14 +11,14 @@ module.exports = {
 
     postUbicacionConductor(req, res){
         const data = req.allParams();
-        sails.sockets.join(req, 'empresa'+data.empresa+'ubicacionWhacher'+data.estacion);
-        sails.sockets.broadcast('empresa'+data.empresa+'ubicacionWhacher'+data.estacion, 'posConductor', req.allParams(), req);
-        sails.log.silly('broadcast empresa'+data.empresa+'ubicacionWhacher'+data.estacion+':posConductor');
+        sails.sockets.join(req, 'empresa'+data.empresa+'tracking');
+        sails.sockets.broadcast('empresa'+data.empresa+'tracking', 'posConductores'+data.estacion, req.allParams(), req);
+        sails.log.silly('broadcast empresa'+data.empresa+'tracking'+':posConductores'+data.estacion);
         res.ok();
     },
 
     findUbicacionConductores(req, res) {
-        sails.sockets.join(req, 'empresa'+req.allParams().id+'ubicacionWhacher'+req.allParams().estacion);
+        sails.sockets.join(req, 'empresa'+req.allParams().id+'tracking');
         res.ok();
     }
 };
