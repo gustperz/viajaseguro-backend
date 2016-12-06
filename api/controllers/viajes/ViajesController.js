9b61dcc6e5f2f115ff1bcc6aa6f74e373e19b541
Gustavo Pérez Machado <gustperz@gmail.com>
2016-12-06 15:44:33 -0500

    save modalidad viajes

--- api/controllers/viajes/ViajesController.js
+++ api/controllers/viajes/ViajesController.js
@@ -54,36 +54,54 @@ module.exports = {
 
     create(req, res){
         var data = req.allParams();
+
+        if(!data.modalidad) return res.badRequest('modalidad required');
+
         async.parallel({
             empresa: cb => {
                 Empresas.findOne(req.user.empresa.id).exec(cb);
             },
             max_contrato: cb => {
-                Viajes.find({empresa: req.user.empresa.id}).max('contrato').exec(cb);
+                Viajes.find({
+                    empresa: req.user.empresa.id,
+                    modalidad: data.modalidad
+                }).max('contrato').exec(cb);
             },
             max_cont_dia: cb => {
                 Viajes.find({
                     empresa: req.user.empresa.id,
+                    modalidad: data.modalidad,
                     fecha: moment().format('YYYY-MM-DD')
                 }).max('cont_dia').exec(cb);
             }
         }, function (err, result) {
             if (err) return res.negotiate(err);
 
-            const territorial = result.empresa.ndireccion_terr;
-            const resolucion = ('0000' + result.empresa.nresolucon).slice(-4);
-            const fecha_resol = moment(result.empresa.fecha_resolucion).format('YY');
-            const fecha_exp = moment().format('YYYY');
-            const contrato = result.max_contrato ? ('000' + (+result.max_contrato[0].contrato + 1)).slice(-4) : '0001';
-            const cont_dia = result.max_cont_dia ? ('000' + (+result.max_cont_dia[0].cont_dia + 1)).slice(-4) : '0001';
+            if (!result.empresa.nresolucon) {
+                return res.notFound('No se han registrado todos los datos de la empresa', {code: 'E_INCOMPLETE_EMPRESA_DATA'});
+            }            
 
-            if (!territorial || !resolucion) return res.notFound('No se han registrado todos los datos de la empresa', {code: 'E_INCOMPLETE_EMPRESA_DATA'});
+            const contrato = result.max_contrato ? ('000' + (+result.max_contrato[0].contrato + 1)).slice(-4) : '0001';
 
             data.contrato = contrato;
-            data.cont_dia = cont_dia;
             data.central = req.user.central.id;
             data.empresa = result.empresa.id;
-            data.fuec = territorial + resolucion + fecha_resol + fecha_exp + contrato + cont_dia;
+
+            if(data.modalidad == 'especial') {    
+                if (!result.empresa.ndireccion_terr) {
+                    return res.notFound('No se han registrado todos los datos de la empresa', {code: 'E_INCOMPLETE_EMPRESA_DATA'});
+                }
+                            
+                const territorial = result.empresa.ndireccion_terr;
+                const resolucion = ('0000' + result.empresa.nresolucon).slice(-4);
+                const fecha_resol = moment(result.empresa.fecha_resolucion).format('YY');
+                const fecha_exp = moment().format('YYYY');
+                const cont_dia = result.max_cont_dia ? ('000' + (+result.max_cont_dia[0].cont_dia + 1)).slice(-4) : '0001';
+
+                data.cont_dia = cont_dia;
+                data.fuec = territorial + resolucion + fecha_resol + fecha_exp + contrato + cont_dia;
+            }
+
             Viajes.create(data).then(viaje => {
 
                 finishSolicitudes(viaje.conductor, viaje.central);
@@ -199,7 +217,7 @@ module.exports = {
             },
             sort: 'fecha ASC'
         }).populate('conductor').populate('vehiculo').populate('empresa').then(datos => {
-            datos.forEach(function(viaje){
+            datos.forEach(function (viaje) {
                 viaje.conductor.fecha_licencia = moment(viaje.conductor.fecha_licencia).format('L');
                 viaje.empresa.fecha_resolucion = moment(viaje.empresa.fecha_resolucion).locale("es").format('LL');
                 viaje.f_contrato = {
@@ -209,12 +227,12 @@ module.exports = {
                 }
                 viajes.push(viaje);
             })
-            if(req.allParams().contrato == 'true'){
+            if (req.allParams().contrato == 'true') {
                 data = {
                     template: {'shortid': 'rJTXRSN7x'},
                     data: viajes
                 }
-            }else {
+            } else {
                 data = {
                     template: {'shortid': 'B144VRaR'},
                     data: viajes
@@ -243,7 +261,7 @@ module.exports = {
             },
             sort: 'fecha ASC'
         }).populate('conductor').populate('vehiculo').populate('empresa').then(datos => {
-            datos.forEach(function(viaje){
+            datos.forEach(function (viaje) {
                 viaje.conductor.fecha_licencia = moment(viaje.conductor.fecha_licencia).format('L');
                 viaje.empresa.fecha_resolucion = moment(viaje.empresa.fecha_resolucion).locale("es").format('LL');
                 viaje.f_contrato = {
